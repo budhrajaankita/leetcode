baa85465cf089d05ab7b137c298407849b6911b5
Ankita Budhraja <budhraja.ankita@gmail.com>
2025-04-02 00:33:02 -0700

    Create file

---
+++ 74-Searcha2DMatrix/version/python3/74-Searcha2DMatrix_20250402_003302.py
@@ -0,0 +1,31 @@
+# Last updated: 4/2/2025, 12:33:02 AM
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+
+        lo = 0
+        hi = len(matrix) - 1
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if target > matrix[mid][0] and target < matrix[mid][-1]:
+                break
+            elif target < matrix[mid][0]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+
+        ROW = (lo + hi) //2
+        l = 0
+        h = len(matrix[ROW]) - 1
+
+        while l <= h:
+            m = (l + h) // 2
+            if target == matrix[ROW][m]:
+                return True
+            elif target < matrix[ROW][m]:
+                h = m - 1
+            else:
+                l = m + 1
+
+        return False
