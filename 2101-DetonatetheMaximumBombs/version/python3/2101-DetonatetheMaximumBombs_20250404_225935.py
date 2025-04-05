cc6f87b747ab1467dd1a3577d7959e1b4ca0034b
Ankita Budhraja <budhraja.ankita@gmail.com>
2025-04-04 22:59:34 -0700

    Create file

---
+++ 2101-DetonatetheMaximumBombs/version/python3/2101-DetonatetheMaximumBombs_20250404_225935.py
@@ -0,0 +1,41 @@
+# Last updated: 4/4/2025, 10:59:35 PM
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+
+        adj = defaultdict(list)
+
+        for i in range(len(bombs)):
+            for j in range(i+1,len(bombs)):
+                x1, y1, r1 = bombs[i]
+                x2, y2, r2 = bombs[j]
+
+                dist = ((x2-x1)**2 + (y2-y1)**2)**0.5
+                if r1 >= dist:
+                    adj[i].append(j)
+
+                if r2 >= dist:
+                    adj[j].append(i)
+        
+        maxBombs = 0
+
+        def dfs(bomb, visited):
+            visited.add(bomb)
+
+            for nei in adj[bomb]:
+                if nei not in visited:
+                    dfs(nei, visited)
+
+            return len(visited)
+            
+
+
+        for i in range(len(bombs)):
+            
+            res = dfs(i,visited=set())
+            maxBombs = max(res, maxBombs)
+        
+        return maxBombs
+
+
+
+        
