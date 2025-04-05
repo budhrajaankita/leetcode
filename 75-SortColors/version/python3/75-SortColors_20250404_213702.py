ae7c344ce39c51b76fa5706db567da40400eefe5
Ankita Budhraja <budhraja.ankita@gmail.com>
2025-04-04 21:37:01 -0700

    Create file

---
+++ 75-SortColors/version/python3/75-SortColors_20250404_213702.py
@@ -0,0 +1,34 @@
+# Last updated: 4/4/2025, 9:37:02 PM
+from collections import Counter
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        zero=0
+        two=len(nums)-1
+        curr =0
+
+        while curr <= two:
+
+            if nums[curr] == 0:
+                nums[curr], nums[zero] = nums[zero], nums[curr]
+                curr += 1
+                zero += 1
+
+            elif nums[curr] == 1:
+                curr += 1
+
+            elif nums[curr] == 2:
+                nums[curr], nums[two] = nums[two], nums[curr]
+                two -= 1
+
+
+2,0,1
+
+1,0,2
+
+
+
+
