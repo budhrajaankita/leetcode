eeeed6c5c419e9ed17905a6d22cc06a6591625c5
Ankita Budhraja <budhraja.ankita@gmail.com>
2025-04-06 00:36:46 -0700

    Create file

---
+++ 5-LongestPalindromicSubstring/version/python3/5-LongestPalindromicSubstring_20250406_003646.py
@@ -0,0 +1,49 @@
+# Last updated: 4/6/2025, 12:36:46 AM
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        if len(s) == 1:
+            return s
+
+        maxPal = ""
+
+        def isPal(l, r):
+            while l > 0 and r < len(s)-1 and s[l-1] == s[r+1]:
+                l -= 1
+                r += 1
+            return l, r, r-l+1
+
+
+        #odd
+        left = right = maxLen = 0
+        for i in range(len(s)-1):
+            l1, r1, maxlen1 = isPal(i,i)
+            if maxlen1 > maxLen:
+                maxLen= maxlen1
+                left=l1
+                right=r1
+
+            if s[i] == s[i+1]:
+                l2, r2, maxlen2 = isPal(i, i+1)
+                if maxlen2 > maxLen:
+                    maxLen= maxlen2
+                    left=l2
+                    right=r2
+
+        return s[left:right+1]
+
+
+            
+            
+            
+
+            
+                
+
+
+
+
+
+
+
+        
